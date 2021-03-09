cb2a6eca998ddd089707d3e504171575c7e25c4e
shengwu <leiyangxili12zuxd@163.com>
2021-03-09 11:47:45 +0800

    spring1

--- yeb-server/src/main/java/com/xxxx/server/config/Test.java
+++
@@ -1,9 +0,0 @@
-package com.xxxx.server.config;
-
-/**
- * @date ：Created in 2021/3/9 10:34
- * @description：测试git
- * @author：
- */
-public class Test {
-}
---
+++ yeb-server/src/main/java/com/xxxx/server/config/security/TestGit.java
@@ -0,0 +1,9 @@
+package com.xxxx.server.config.security;
+
+/**
+ * @date ：Created in 2021/3/9 11:46
+ * @description：测试
+ * @author：
+ */
+public class TestGit {
+}
