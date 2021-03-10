1da423641da993fdee4a0327f3c5940198aaba46
shengwu <leiyangxili12zuxd@163.com>
2021-03-10 17:17:35 +0800

    加main

--- yeb-server/src/main/java/com/xxxx/server/config/security/component/Git.java
+++ yeb-server/src/main/java/com/xxxx/server/config/security/component/Git.java
@@ -1,12 +1,15 @@
 package com.xxxx.server.config.security.component;
 
 /**
- * @date ：Created in 2021/3/10 16:51
- * @description：git
- * @author：
+ * @date &#xFF1A;Created in 2021/3/10 16:51
+ * @description&#xFF1A;git
+ * @author&#xFF1A;
  */
 public class Git {
 // 主方法增加
 //更新
+public static void main(String[] args) {
+
+}
  }
 
