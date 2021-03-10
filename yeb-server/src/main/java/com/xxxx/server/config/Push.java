84528b8db0cdcbf9a59a72eb05862b41b515744c
shengwu <leiyangxili12zuxd@163.com>
2021-03-10 16:36:50 +0800

    删除

--- yeb-server/src/main/java/com/xxxx/server/config/Push.java
+++
@@ -1,9 +0,0 @@
-package com.xxxx.server.config;
-
-/**
- * @date ：Created in 2021/3/10 11:57
- * @description：推送文件
- * @author：
- */
-public class Push {
-}
--- yeb-server/src/main/java/com/xxxx/server/config/security/component/gf.java
+++
@@ -1,9 +0,0 @@
-package com.xxxx.server.config.security.component;
-
-/**
- * @date ：Created in 2021/3/10 16:21
- * @description：框架
- * @author：
- */
-public class gf {
-}
