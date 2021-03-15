d533845e80a473657c2713aff0533ed873e015ef
shengwu <leiyangxili12zuxd@163.com>
2021-03-15 17:47:12 +0800

    PositionController.java

--- yeb-server/src/main/java/com/xxxx/server/pojo/Position.java
+++ yeb-server/src/main/java/com/xxxx/server/pojo/Position.java
@@ -3,6 +3,9 @@ package com.xxxx.server.pojo;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.fasterxml.jackson.annotation.JsonFilter;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonAppend;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
@@ -37,6 +40,7 @@ public class Position implements Serializable {
     private String name;
 
     @ApiModelProperty(value = "创建时间")
+    @JsonFormat(pattern = "yyyy-mm-dd",timezone = "Asia/Shanghai")
     private LocalDateTime createDate;
 
     @ApiModelProperty(value = "是否启用")
